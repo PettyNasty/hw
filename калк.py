f3aaa32c786c4591a426e0c9af301dd0eb8e3bb4
PettyNasty <145860494+PettyNasty@users.noreply.github.com>
2023-09-24 20:04:08 +0300

    Add files via upload

---
+++ калк.py
@@ -0,0 +1,11 @@
+a = int(input("Число 1"))
+b = int(input("Число 2"))
+c = int(input("Число 3"))
+d = int(input("Число 4"))
+e = int(input("Число 5"))
+f = a * b * c
+g = d/e
+h = f * g
+i = f/g
+j = h+i
+print(j)
